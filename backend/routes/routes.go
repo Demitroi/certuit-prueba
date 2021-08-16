515ba1730791a4926336c27e5771528ac30a254c
Sergio contreras <sergio@saitenlinea.com>
2021-08-16 14:58:30 -0700

    Validar token de sesion

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -10,9 +10,30 @@ func SetupRoutes() *gin.Engine {
 	r := gin.Default()
 	v1 := r.Group("/api/presupuestos/v1")
 	{
+		// Rutas publicas
 		public := v1.Group("public")
 		{
 			public.POST("login", controllers.Login)
+		}
+		// Rutas con autenticación
+		auth := v1.Group("")
+		auth.Use(func(c *gin.Context) {
+			// Obtener el token del header
+			token := c.GetHeader("x-Token")
+			usuario, err := controllers.Verify(token)
+			if err != nil {
+				c.AbortWithStatusJSON(401, gin.H{"msg": "No autorizado"})
+				return
+			}
+			// Establecer la información del usuario
+			c.Set("usuario", usuario)
+			c.Next()
+		})
+		{
+			auth.GET("sesion", func(c *gin.Context) {
+				usuario, _ := c.Get("usuario")
+				c.JSON(200, usuario)
+			})
 		}
 	}
 	return r
